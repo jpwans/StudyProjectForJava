b403fc6e9a4de90c955a92fa6ef623abe38a5330
mopellet <351420450@qq.com>
2017-03-01 18:21:31 +0800

    泛型

---
+++ StudyTestProject/src/com/pair1/Pair.java
@@ -0,0 +1,28 @@
+package com.pair1;
+
+/**
+ * Created by jiangqin on 17/3/1.
+ */
+public class Pair<T> {
+    private T first;
+    private T second;
+
+    public Pair(){first = null; second = null;}
+    public Pair(T first,T second){this.first = first; this.second = second;}
+
+    public T getFirst() {
+        return first;
+    }
+
+    public T getSecond() {
+        return second;
+    }
+
+    public void setFirst(T first) {
+        this.first = first;
+    }
+
+    public void setSecond(T second) {
+        this.second = second;
+    }
+}
