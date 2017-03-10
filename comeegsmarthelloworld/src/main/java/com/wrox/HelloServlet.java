b43fae754220c95ac8086d2efd03ae75dfae8d47
mopellet <351420450@qq.com>
2017-03-10 18:49:40 +0800

    更新web

--- comeegsmarthelloworld/src/main/java/com/wrox/HelloServlet.java
+++ comeegsmarthelloworld/src/main/java/com/wrox/HelloServlet.java
@@ -1,19 +1,56 @@
 package com.wrox;
 
+import javax.jws.WebService;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * Created by jiangqin on 17/3/9.
  */
+
+@WebServlet(
+        name = "helloServlet",
+        urlPatterns = {"/greeting","/salutation","/wazzup"},
+        loadOnStartup = 1
+)
+
 public class HelloServlet extends HttpServlet {
+    private static final String DEFAULT_USER = "Guest";
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("resp : "  + req.toString());
-        resp.getWriter().println("hello world");
+        String user = req.getParameter("user");
+        if (user == null)
+            user = HelloServlet.DEFAULT_USER;
+
+        resp.setContentType("text/html");
+        resp.setCharacterEncoding("UTF-8");
+
+        PrintWriter writer = resp.getWriter();
+        writer.append("<!DOCTYPE html>\r\n")
+                .append("<html>\r\n")
+                .append("   <head>\r\n")
+                .append("       <title>Hello User Application</title>\r\n")
+                .append("   </head>\r\n")
+                .append("   <body>\r\n")
+                .append("       Hello, ").append(user).append("!<br/><br/>\r\n")
+                .append("       <form action=\"greeting\" method=\"POST\">\r\n")
+                .append("           Enter your name:<br/>\r\n")
+                .append("           <input type=\"text\" name=\"user\"/><br/>\r\n")
+                .append("           <input type=\"submit\" value=\"Submit\"/>\r\n")
+                .append("       </form>\r\n")
+                .append("   </body>\r\n")
+                .append("<html>\r\n");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doGet(req,resp);
     }
 
     @Override
